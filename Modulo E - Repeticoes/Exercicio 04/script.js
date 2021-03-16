ff9a903bdc581ab97147e994a14e8418f9479a5d
VictorOliveira02 <victoralvees17@gmail.com>
2021-03-16 10:44:33 -0300

    finished module E and add exercise 4

--- Modulo E - Repeticoes/Exercicio 04/script.js
+++ Modulo E - Repeticoes/Exercicio 04/script.js
@@ -1,26 +1,19 @@
-function contar() {
-    let inicio = document.getElementById('inicio')
-    let fim = document.getElementById('fim')
-    let passo = document.getElementById('passo')
+function tabuada() {
+    let num = document.getElementById('num')
+    let tab = document.getElementById('tab')
 
-    let result = document.getElementById('result')
+    if (num.value.length == 0) {
+        alert('Por Favor, digite os valores corretamente!')
 
-    if (inicio.value.length == 0 || fim.value.length == 0 | passo.value.length == 0) {
-        alert("Por Favor, digite os dados corretamente!");
     } else {
-        result.innerHTML = `Contando: `
-        let start = Number(inicio.value)
-        let finish = Number(fim.value)
-        let step = Number(passo.value)
-        if (start < finish) {
-            for (let cont = start; cont <= finish; cont += step) {
-                result.innerHTML += `${cont} <strong>-></strong> `
-            }
-        } else {
-            for (let cont = start; cont >= finish; cont -= step) {
-                result.innerHTML += `${cont} <strong>-></strong> `
-            }
+        let n = Number(num.value)
+        let cont = 1
+        tab.innerHTML = ''
+        while (cont <= 10) {
+            let item = document.createElement('option')
+            item.text = ` ${n} x ${cont} = ${n * cont}`
+            tab.appendChild(item)
+            cont++
         }
-        result.innerHTML += `<strong>FIM!</strong>`
     }
 }
